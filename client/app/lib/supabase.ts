2f52afe04cfa5efac04dccc043442c4e99a0c702
Oladayo Ahmod <57647734+Oladayo-Ahmod@users.noreply.github.com>
2026-02-03 03:21:06 +0000

    fix : supabase config

--- client/app/lib/supabase.ts
+++ client/app/lib/supabase.ts
@@ -1,10 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-export const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+ const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+ const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY!
 
 export const createSupabaseClient = () => {
-  return createClient(supabaseUrl, supabaseAnonKey);
+  return createClient(supabaseUrl,supabaseKey);
 };
 
 // Helper functions
